23fb14f2e1d7c8609299506befc9e373753178d1
Daniel Moniz <daniel.moniz@gmail.com>
2015-09-29 13:15:03 -0400

    Allow cards to be used as the backs of other cards.

--- static/js/format.js
+++ static/js/format.js
@@ -44,58 +44,64 @@ $(function() {
     var current_page = get_page('front');
     current_page.appendTo(current_container);
 
-    var last_width = undefined;
-    var last_height = undefined;
-    start_index = 0
+    if (backs.length > 0) {
+      var back_page = get_page('back');
+      var back_container = get_container('back');
+      back_container.append(back_page);
+    }
 
     for (var i=0; i<fronts.length; i++) {
-      var card = fronts[i];
-      last_width = $(card).width();
-      last_height = $(card).height();
-      format_card(card);
-      current_page.append(card);
+      var front = fronts[i];
+      var back = undefined;
+      if (backs.length > 0) {
+        back = backs[i];
+      }
+
+      // set card as previous card's back if needed
+      if ($(front).find('.placement').attr('placement') == 'back') {
+        var last_back = $(backs[i - 1]);
+        last_back.replaceWith(front);
+        back.remove();
+        continue;
+      }
+
+      current_page.append(front);
+      format_card(front);
+      if (back) {
+        add_back(back, back_page, front);
+      }
 
       if (current_page.height() > current_container.height()) {
-        card.remove();
-
-        if (backs.length > 0) {
-          var back_page = add_back_page(backs, start_index, i);
-          var back_container = get_container('back');
-          back_container.append(back_page);
-          start_index = i;
+        front.remove();
+        if (back) {
+          back.remove();
         }
 
         current_container = get_container('front');
         current_page = get_page('front');
-        current_page.append(card)
+        current_page.append(front)
         current_container.append(current_page);
-      }
-    }
 
-    if (backs.length > 0) {
-      var back_page = add_back_page(backs, start_index, backs.length);
-      var back_container = get_container('back');
-      back_container.append(back_page);
+        if (backs.length > 0) {
+          var back_container = get_container('back');
+          var back_page = get_page('back');
+          back_page.append(back);
+          back_container.append(back_page);
+        }
+      }
     }
   }
 
-  function add_back_page(backs, start, end) {
-    var page = get_page('back');
-    for (var i=start; i<end; i++) {
-      var card = backs[i];
-      var back_text = $.trim($(card).text());
-
-      if (back_text == 'SAME AS FRONT') {
-        var front_page = $('.page.front:last');
-        var index = i % front_page.children().length + 1;
-        var front_card = front_page.children().filter(':nth-child('+ index +')');
-        page.append(front_card.clone());
-        card.remove();
-      } else {
-        page.append(card);
-      }
+  function add_back(back, page, front) {
+    var back_text = $.trim($(back).text());
+    if (back_text != 'SAME AS FRONT') {
+      page.append(back);
+      format_card(back);
+      return;
     }
-    return page;
+
+    page.append($(front).clone());
+    back.remove();
   }
 
 });
